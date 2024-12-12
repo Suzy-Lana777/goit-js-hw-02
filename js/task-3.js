d3e5aa5d324d4ca9ac0ff9b5b86a8d41cbc1a31b
Suzy-Lana777 <reutasvetlana382@gmail.com>
2024-12-12 21:46:31 +0200

    home work2

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,16 @@
-function getElementWidth(content, padding, border) {
-  const totalWidth = parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-  return totalWidth;
+function checkForSpam(message) {
+  const word1 = 'spam';
+  const word2 = 'sale';
+  const wordLower = message.toLowerCase();
+  const banWord1 = wordLower.includes(word1);
+  const banWord2 = wordLower.includes(word2);
+  const banWords = banWord1 || banWord2;
+  return banWords;
 }
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
